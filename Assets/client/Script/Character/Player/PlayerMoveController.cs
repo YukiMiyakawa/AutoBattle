57ec179f461698abb51397a1a16c03eadeddaa07
YukiMiyakawa <sakomiya3838@gmail.com>
2024-09-29 07:56:54 +0900

    行動処理分離

---
+++ Assets/client/Script/Character/Player/PlayerMoveController.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Character.Common.CommonCharacterController;
+
+namespace Character.Player
+{
+    public class PlayerMoveController : MonoBehaviour
+    {
+        [Header("移動")]
+        [SerializeField] private float _addHorizontalRate = 20f;
+        [SerializeField] private float _addVerticalRate = 20f;
+        [Header("ジャンプ")]
+        [SerializeField] private float _addJumpForce = 1f;
+        [SerializeField] private float _jumpAttenuationRate = 0.1f;
+        [SerializeField] private float _checkDistance = 0.1f; // チェックする距離
+        [SerializeField] private LayerMask _groundLayer; // 地面のレイヤー
+
+        // ジャンプ
+        private float _jumpForceCash;
+        private bool _onJump;
+        public bool OnJump => _onJump;
+
+        private Rigidbody _playerRigidbody;
+
+        private void Awake()
+        {
+            _jumpForceCash = _addJumpForce;
+        }
+
+
+        public void Initialize(Rigidbody rb)
+        {
+            _playerRigidbody = rb;
+        }
+
+        /// <summary>
+        /// 歩き
+        /// </summary>
+        public void OnMove(Vector3 forward)
+        {
+            if (forward == default)
+            {
+                return;
+            }
+
+            var moveAmount = new Vector3(forward.x * _addHorizontalRate, forward.y, forward.z * _addVerticalRate);
+            _playerRigidbody.AddForce(moveAmount);
+        }
+
+        /// <summary>
+        /// ジャンプフラグオン
+        /// </summary>
+        public void SetOnJump()
+        {
+            _onJump = true;
+        }
+
+        /// <summary>
+        /// 着地したかどうか
+        /// </summary>
+        public bool IsGrounded()
+        {
+            return Physics.Raycast(transform.position, Vector3.down, _checkDistance)
+                && !_onJump;
+        }
+
+        // NOTE : ジャンプ時キャラ回転を防ぐためConstraints　Rotationに制限をかける
+        public void UpdateOnJump()
+        {
+            if (_onJump)
+            {
+                if (_jumpForceCash <= 0)
+                {
+                    _playerRigidbody.useGravity = true;
+                    _jumpForceCash = _addJumpForce;
+                    _onJump = false;
+
+                    // ジャンプ方向ベロシティを0に
+                    var tmpVelosity = _playerRigidbody.velocity;
+                    tmpVelosity.y = 0;
+                    _playerRigidbody.velocity = tmpVelosity;
+
+                    return;
+                }
+                else if (_jumpForceCash == _addJumpForce)
+                {
+                    _playerRigidbody.useGravity = false;
+                }
+
+                _jumpForceCash -= _jumpAttenuationRate;
+                _playerRigidbody.AddForce(new Vector3(0, _jumpForceCash, 0));
+            }
+        }
+    }
+}
